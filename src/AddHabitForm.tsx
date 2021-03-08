c0b48081f9dc17448ddc266138960bd0305e4ded
JohannesStroebele <stroebele.j@gmail.com>
2021-03-08 10:07:35 +0100

    Setup handler for change in the text input

--- src/AddHabitForm.tsx
+++ src/AddHabitForm.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 export const AddHabitForm: React.FC = () => {
     const [newHabit, setNewHabit] = useState<string>();
 
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewHabit((event.target.value));
+    }
+
     return (
         <form>
-            <input type="text" value={newHabit}/>
+            <input type="text" value={newHabit} onChange={handleChange}/>
             <button type="submit">Add Habit</button>
         </form>
     )
